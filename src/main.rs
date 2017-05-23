b045a9fcbc445d2eb77baf186b8427f6e9621177
Kirill Khazan <kirillkh@gmail.com>
2017-05-23 14:52:48 +0300

    fast up to N=50

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 extern crate rand;
 
-use rand::{weak_rng, XorShiftRng, Rng};
+#[macro_use]
+extern crate lazy_static;
+
+
+use rand::{XorShiftRng, Rng, thread_rng, SeedableRng};
 use std::fmt;
 use std::fmt::{Formatter, Debug};
+
+use std::sync::Mutex;
 
 trait Specimen: Sized {
     fn score(&self) -> f32;
@@ -15,12 +21,11 @@ trait Specimen: Sized {
 }
 
 
-
-fn genetic<S: Specimen>(threshold: f32, max_iters: usize, nparents: usize) -> S {
+fn genetic<S: Specimen>(threshold: f32, max_iters: usize, nparents: usize, nchildren: usize) -> S {
     let mut species: Vec<S> = S::initial();
     let mut best = (0f32, 0);
     for _ in 0..max_iters {
-        species = next_gen(species, nparents);
+        species = next_gen(species, nparents, nchildren);
         
         for (i, specimen) in species.iter().enumerate() {
             let score = specimen.score();
@@ -38,43 +43,54 @@ fn genetic<S: Specimen>(threshold: f32, max_iters: usize, nparents: usize) -> S 
     species.swap_remove(best.1)
 }
 
-fn next_gen<S: Specimen>(mut species: Vec<S>, nparents: usize) -> Vec<S> {
+#[inline(never)]
+fn next_gen<S: Specimen>(mut species: Vec<S>, nparents: usize, nchildren: usize) -> Vec<S> {
     // 1. filter strongest
     S::filter_strongest(&mut species);
     
     // 2. bear children
-    let families = partition(species, nparents);
-    let mut species = vec![];
-    let mut children = vec![];
-    for family in families.into_iter() {
-        if family.len() > 1 {
-            let child = S::procrastinate(&family);
-            children.push(child);
+    let mut all_children = vec![];
+    for _ in 0..nchildren {
+        let families = partition(species, nparents);
+        species = vec![];
+        let mut children = vec![];
+        for family in families.into_iter() {
+            if family.len() > 1 {
+                //            for _ in 0..nchildren {
+                let child = S::procrastinate(&family);
+                children.push(child);
+                //            }
+            }
+            species.extend(family)
         }
-        species.extend(family)
+    
+        // 3. mutate
+        S::mutate(&mut children);
+        for child in children.iter_mut() {
+            child.reevaluate();
+        }
+    
+        all_children.extend(children);
     }
     
-    // 3. mutate
-    S::mutate(&mut children);
-    for child in children.iter_mut() {
-        child.reevaluate();
+    if KILL_PARENTS {
+        all_children
+    } else {
+        species.extend(all_children);
+        species
     }
-    
-    species.extend(children);
-    
-    species
 }
 
 fn partition<S: Specimen>(mut species: Vec<S>, nparents: usize) -> Vec<Vec<S>> {
     let mut families = vec![];
     let mut family = vec![];
-    let mut rng = weak_rng();
     let mut j = 0;
+    let mut rng = RNG.lock().unwrap();
     for _ in 0..species.len() {
         let next = rng.gen_range(0, species.len());
         let s = species.swap_remove(next);
         family.push(s);
-        
+    
         j += 1;
         if j == nparents {
             j = 0;
@@ -82,11 +98,11 @@ fn partition<S: Specimen>(mut species: Vec<S>, nparents: usize) -> Vec<Vec<S>> {
             family = vec![];
         }
     }
-    
+
     if !family.is_empty() {
         families.push(family);
     }
-    
+
     families
 }
 
@@ -94,25 +110,63 @@ fn partition<S: Specimen>(mut species: Vec<S>, nparents: usize) -> Vec<Vec<S>> {
 //---------------------------------------------------
 
 // BEST FOR SIZE=8
+//const KILL_PARENTS: bool = true;
 //const SIZE: usize = 8;
-//const MUTATION_PROBABILITY: f32 = 0.01f32;
-//const POPULATION: usize = 500;
-//const MAX_ITERS: usize = 100000;
-//const NPARENTS: usize = 3;
+//const MUTATION_PROBABILITY: f32 = 1.0f32;
+//const POPULATION: usize = 3;
+//const MAX_ITERS: usize = 1000;
+//const NPARENTS: usize = 2;
+//const NCHILDREN: usize = 100;
 
-
+// BEST FOR SIZE=12
+//const KILL_PARENTS: bool = true;
 //const SIZE: usize = 12;
-//const MUTATION_PROBABILITY: f32 = 0.01f32;
-//const POPULATION: usize = 1000;
-//const MAX_ITERS: usize = 100000;
-//const NPARENTS: usize = 3;
+//const MUTATION_PROBABILITY: f32 = 1.0f32;
+//const POPULATION: usize = 3;
+//const MAX_ITERS: usize = 1000;
+//const NPARENTS: usize = 2;
+//const NCHILDREN: usize = 100;
+
+// BEST FOR SIZE=15
+//const KILL_PARENTS: bool = true;
+//const SIZE: usize = 15;
+//const MUTATION_PROBABILITY: f32 = 1.0f32;
+//const POPULATION: usize = 4;
+//const MAX_ITERS: usize = 1000;
+//const NPARENTS: usize = 2;
+//const NCHILDREN: usize = 140;
+
+// BEST FOR SIZE=30
+//const KILL_PARENTS: bool = true;
+//const SIZE: usize = 30;
+//const MUTATION_PROBABILITY: f32 = 1.0f32;
+//const POPULATION: usize = 4;
+//const MAX_ITERS: usize = 5000;
+//const NPARENTS: usize = 2;
+//const NCHILDREN: usize = 240;
+
+// BEST FOR SIZE=50
+//const KILL_PARENTS: bool = true;
+//const SIZE: usize = 50;
+//const MUTATION_PROBABILITY: f32 = 1.0f32;
+//const POPULATION: usize = 5;
+//const MAX_ITERS: usize = 5000;
+//const NPARENTS: usize = 2;
+//const NCHILDREN: usize = 320;
+
+const KILL_PARENTS: bool = true;
+const SIZE: usize = 100;
+const MUTATION_PROBABILITY: f32 = 1.0f32;
+const POPULATION: usize = 5;
+const MAX_ITERS: usize = 5000;
+const NPARENTS: usize = 2;
+const NCHILDREN: usize = 320;
 
 
-const SIZE: usize = 12;
-const MUTATION_PROBABILITY: f32 = 0.001f32;
-const POPULATION: usize = 2000;
-const MAX_ITERS: usize = 100000;
-const NPARENTS: usize = 3;
+
+lazy_static! {
+    static ref RNG: Mutex<XorShiftRng> = Mutex::new(XorShiftRng::new_unseeded());
+}
 
 
 struct Board {
@@ -131,15 +185,11 @@ impl Board {
         Board { score: 0f32, queens: queens, cells: cells }
     }
     
-//    fn n(&self) -> usize {
-//        SIZE
-//    }
-    
     fn at(&mut self, x: usize, y: usize) -> &mut bool {
-//        let n = self.n();
         &mut self.cells[SIZE * y + x]
     }
     
+    #[inline(never)]
     fn do_mutate(&mut self, rng: &mut XorShiftRng) {
         let q = rng.gen_range(0, SIZE);
         loop {
@@ -150,7 +200,6 @@ impl Board {
                 *self.at(oldx, oldy) = false;
                 *self.at(x, y) = true;
                 self.queens[q] = (x, y);
-//                self.reevaluate();
                 break;
             }
         }
@@ -177,10 +226,11 @@ impl Board {
 }
 
 impl Specimen for Board {
+    #[inline(never)]
     fn reevaluate(&mut self) {
         let mut score: usize = 0;
         let mut conflicts = vec![0; SIZE];
-        for i in 0 .. self.queens.len() {
+        for i in 0 .. SIZE {
             self.eval_conflicts(i, &mut conflicts);
             
             if conflicts[i] == 0 {
@@ -195,8 +245,9 @@ impl Specimen for Board {
         self.score
     }
     
+    #[inline(never)]
     fn mutate(new_gen: &mut [Self]) {
-        let mut rng = weak_rng();
+        let mut rng = RNG.lock().unwrap();
         for board in new_gen.iter_mut() {
             if rng.next_f32() < MUTATION_PROBABILITY {
                 board.do_mutate(&mut rng);
@@ -204,21 +255,22 @@ impl Specimen for Board {
         }
     }
     
+    #[inline(never)]
     fn procrastinate(parents: &[Self]) -> Self {
-        let mut rng = weak_rng();
         let nqueens = parents.len() * SIZE;
         
-        let mut child = Board::new(SIZE, vec![]);
+        let mut child = Board::new(SIZE, Vec::with_capacity(SIZE));
         
         let mut all_queens = (0..nqueens).collect::<Vec<_>>();
-        
+    
+        let mut rng = RNG.lock().unwrap();
         loop {
             let next = rng.gen_range(0, all_queens.len());
             let mut queen = all_queens.swap_remove(next);
-            let parent = queen/SIZE;
+            let parent = queen / SIZE;
             queen %= SIZE;
-            
-            let (x,y) = parents[parent].queens[queen].clone();
+    
+            let (x, y) = parents[parent].queens[queen].clone();
             if !*child.at(x, y) {
                 child.queens.push((x, y));
                 *child.at(x, y) = true;
@@ -231,6 +283,7 @@ impl Specimen for Board {
         child
     }
     
+    #[inline(never)]
     fn filter_strongest(species: &mut Vec<Self>) {
         if species.len() <= POPULATION {
             return;
@@ -240,10 +293,11 @@ impl Specimen for Board {
         species.truncate(POPULATION);
     }
     
+    #[inline(never)]
     fn initial() -> Vec<Self> {
-        let mut rng = weak_rng();
         let mut boards = Vec::with_capacity(POPULATION);
-        
+    
+        let mut rng = RNG.lock().unwrap();
         for _ in 0..POPULATION {
             let mut board = Board::new(SIZE, vec![]);
     
@@ -262,7 +316,6 @@ impl Specimen for Board {
             board.reevaluate();
             boards.push(board);
         }
-        
         boards
     }
 }
@@ -275,7 +328,13 @@ impl Debug for Board {
 
 
 fn genetic_queens() {
-    let best = genetic::<Board>(SIZE as f32, MAX_ITERS, NPARENTS);
+    {
+        let mut rng = RNG.lock().unwrap();
+        let mut trng = thread_rng();
+        rng.reseed([trng.next_u32(), trng.next_u32(), trng.next_u32(), trng.next_u32()]);
+    }
+    
+    let best = genetic::<Board>(SIZE as f32, MAX_ITERS, NPARENTS, NCHILDREN);
     println!("best: {:?}", &best);
 }
 
